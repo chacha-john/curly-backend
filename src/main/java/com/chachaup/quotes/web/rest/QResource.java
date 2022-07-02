99a3bd8effb0f3bad616dbcd9daa03226694465e
chacha <chachaerickjo@gmail.com>
2022-07-02 17:59:37 +0300

    fix - add mysql dependency and add requestbody annotation to the save request

--- src/main/java/com/chachaup/quotes/web/rest/QResource.java
+++ src/main/java/com/chachaup/quotes/web/rest/QResource.java
@@ -2,23 +2,27 @@ package com.chachaup.quotes.web.rest;
 
 import com.chachaup.quotes.domain.Quote;
 import com.chachaup.quotes.service.QService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@RestController @RequiredArgsConstructor @Slf4j @RequestMapping("/quotes")
+@RestController @AllArgsConstructor
+@Slf4j @RequestMapping("/quotes")
 public class QResource {
     private final QService qService;
 
     @GetMapping("/all")
     public List<Quote> all(){
+        List<Quote> quotes = new ArrayList<>();
         return qService.getAll();
     }
 
     @PostMapping("/save")
-    public Quote save(Quote quote){
+    public Quote save(@RequestBody Quote quote){
         return qService.add(quote);
     }
 
